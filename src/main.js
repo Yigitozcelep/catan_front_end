d7a33eadd23ff60954d8f8ccb785cac371fb8cab
Yigit Ozcelep <yoroso85onurlu@outlook.com>
2023-09-04 11:58:14 +0300

    add ports

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import create_map from "./map/map_creation.js";
 const invoke = window.__TAURI__.invoke
 
 invoke('get_random_map').then((message) => {
+    console.log(message);
     let res = [];
     for (let i = 0; i < 18; i++) res.push({"num": 0, "resource": "wool", "hexagon_type": "water"})
     let data = eval(message.toLowerCase());
